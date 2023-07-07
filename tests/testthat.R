d371df9a51529482fd801902117527ea5ad7cfbc
Tomeriko96 <IwanTomer@hotmail.com>
2023-07-07 15:12:39 +0200

    update: testthat.R
    remove library calls, instead add packages to DESCRIPTION file in Suggests

--- tests/testthat.R
+++ tests/testthat.R
@@ -6,7 +6,4 @@
 # * https://r-pkgs.org/testing-design.html#sec-tests-files-overview
 # * https://testthat.r-lib.org/articles/special-files.html
 
-library(testthat)
-library(vvshiny)
-
 test_check("vvshiny")
