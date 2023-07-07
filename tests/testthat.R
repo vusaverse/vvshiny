19615878ae2b8399972c8fc464320c86d814e676
Tomeriko96 <IwanTomer@hotmail.com>
2023-07-07 15:16:49 +0200

    fix: testthat.R
    Need to call test_check with the namespace

--- tests/testthat.R
+++ tests/testthat.R
@@ -6,4 +6,4 @@
 # * https://r-pkgs.org/testing-design.html#sec-tests-files-overview
 # * https://testthat.r-lib.org/articles/special-files.html
 
-test_check("vvshiny")
+testthat::test_check("vvshiny")
