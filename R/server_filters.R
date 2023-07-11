b5c683486655a8404c936866c79dc3a7f30d40b0
Corneel den Hartogh <c.f.den.hartogh@vu.nl>
2023-07-11 12:05:33 +0200

    fix: incommented functions and updated filter_with_lists() function to remove <<-

--- R/server_filters.R
+++ R/server_filters.R
@@ -14,63 +14,63 @@
 #' #' @param facet_name_var Variable used for facet grid naming ("VIS_Groep_naam" by default).
 #' #' @return A prepared dataframe with applied filters and new columns.
 #' #' @export
-#' prep_df <- function(lFilters, lValues_for_naming, df, color_var, facet = "left", facet_var = rlang::sym("VIS_Groep"), facet_name_var = rlang::sym("VIS_Groep_naam")) {
-#'   ## Collapses values from the naming list into a single string
-#'   sName <- paste(keep_values(lValues_for_naming), collapse = " / ")
-#'
-#'   ## Removes null elements from the filter list
-#'   lFilters <- lFilters %>%
-#'     purrr::discard(is.null)
-#'
-#'   ## Transforms filter list into elements suitable for filtering
-#'   lFilter_elements <- purrr::map(lFilters, transform_input)
-#'
-#'   ## Applies filters, adds new columns, and casts var used as color to factor
-#'   dfPrepared <- df %>%
-#'     filter_with_lists(lFilter_elements) %>%
-#'     dplyr::mutate(
-#'       !!facet_var := facet,
-#'       !!facet_name_var := paste("VU",
-#'         sName,
-#'         sep = " - "
-#'       )
-#'     ) %>%
-#'     dplyr::mutate(!!rlang::sym(color_var) := as.factor(!!rlang::sym(color_var)))
-#'
-#'   return(dfPrepared)
-#' }
+prep_df <- function(lFilters, lValues_for_naming, df, color_var, facet = "left", facet_var = rlang::sym("VIS_Groep"), facet_name_var = rlang::sym("VIS_Groep_naam")) {
+  ## Collapses values from the naming list into a single string
+  sName <- paste(keep_values(lValues_for_naming), collapse = " / ")
 
-#' #' Keep only relevant values
-#' #'
-#' #' Filters out only relevant values based on the provided filters
-#' #'
-#' #' This function removes null elements from the filter list, transforms filter list into elements
-#' #' suitable for filtering, and retrieves relevant values from the data.
-#' #' @param lFilters List of filters to be applied on the data.
-#' #' @param sVariable The variable for which relevant values are to be retrieved.
-#' #' @param dfFilters Dataframe with the possible filters and values for this dataset
-#' #' @return A list of relevant values for the specified variable.
-#' #' @export
-#' keep_only_relevant_values <- function(lFilters, sVariable, dfFilters) {
-#'   ## Verifies the input variables are set, if not stop execution
-#'   shiny::req(lFilters)
+  ## Removes null elements from the filter list
+  lFilters <- lFilters %>%
+    purrr::discard(is.null)
+
+  ## Transforms filter list into elements suitable for filtering
+  lFilter_elements <- purrr::map(lFilters, transform_input)
+
+  ## Applies filters, adds new columns, and casts var used as color to factor
+  dfPrepared <- df %>%
+    filter_with_lists(lFilter_elements) %>%
+    dplyr::mutate(
+      !!facet_var := facet,
+      !!facet_name_var := paste("VU",
+        sName,
+        sep = " - "
+      )
+    ) %>%
+    dplyr::mutate(!!rlang::sym(color_var) := as.factor(!!rlang::sym(color_var)))
+
+  return(dfPrepared)
+}
+
+#' Keep only relevant values
 #'
-#'   ## Removes null elements from the filter list
-#'   lFilters <- lFilters %>%
-#'     purrr::discard(is.null)
+#' Filters out only relevant values based on the provided filters
 #'
-#'   ## Transforms filter list into elements suitable for filtering
-#'   lFilter_elements <- purrr::map(lFilters, transform_input)
-#'
-#'   ## Retrieves relevant values from the data
-#'   lRelevant_values <- dfFilters %>%
-#'     filter_with_lists(lFilter_elements) %>%
-#'     dplyr::pull(!!rlang::sym(sVariable)) %>%
-#'     purrr::set_names(.) %>%
-#'     purrr::map(~ paste(.x, sVariable, sep = ";"))
-#'
-#'   return(lRelevant_values)
-#' }
+#' This function removes null elements from the filter list, transforms filter list into elements
+#' suitable for filtering, and retrieves relevant values from the data.
+#' @param lFilters List of filters to be applied on the data.
+#' @param sVariable The variable for which relevant values are to be retrieved.
+#' @param dfFilters Dataframe with the possible filters and values for this dataset
+#' @return A list of relevant values for the specified variable.
+#' @export
+keep_only_relevant_values <- function(lFilters, sVariable, dfFilters) {
+  ## Verifies the input variables are set, if not stop execution
+  shiny::req(lFilters)
+
+  ## Removes null elements from the filter list
+  lFilters <- lFilters %>%
+    purrr::discard(is.null)
+
+  ## Transforms filter list into elements suitable for filtering
+  lFilter_elements <- purrr::map(lFilters, transform_input)
+
+  ## Retrieves relevant values from the data
+  lRelevant_values <- dfFilters %>%
+    filter_with_lists(lFilter_elements) %>%
+    dplyr::pull(!!rlang::sym(sVariable)) %>%
+    purrr::set_names(.) %>%
+    purrr::map(~ paste(.x, sVariable, sep = ";"))
+
+  return(lRelevant_values)
+}
 
 
 
@@ -111,21 +111,24 @@ transform_input <- function(input) {
 }
 
 
-#' #' Filter with lists
-#' #'
-#' #' This function filters a dataframe using a list with column and one or more values.
-#' #'
-#' #' @param df A dataframe to be filtered
-#' #' @param filters A list containing column names and their corresponding values for filtering
-#' #'
-#' #' @return A dataframe filtered based on the input filters
-#' #' @export
-#' filter_with_lists <- function(df, filters) {
-#'   ## Applies each filter to the dataframe
-#'   purrr::walk(filters, function(.x) df <<- df %>% dplyr::filter(!!rlang::sym(.x[[1]]) %in% .x[[2]]))
+#' Filter with lists
 #'
-#'   return(df)
-#' }
+#' This function filters a dataframe using a list with column and one or more values.
+#'
+#' @param df A dataframe to be filtered
+#' @param filters A list containing column names and their corresponding values for filtering
+#'
+#' @return A dataframe filtered based on the input filters
+#' @export
+filter_with_lists <- function(df, filters) {
+  ## Applies each filter to the dataframe
+    df <- purrr::reduce(filters, function(df, filter) {
+      df %>% dplyr::filter(!!rlang::sym(filter[[1]]) %in% unlist(filter[[2]]))
+    }, .init = df)
+
+
+  return(df)
+}
 
 
 #' Limit number of values for Gantt chart
