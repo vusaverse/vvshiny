6388a8e0de4882ca25a1d9266a7b6657f80d7038
Corneel den Hartogh <c.f.den.hartogh@vu.nl>
2023-07-12 17:39:44 +0200

    add: gantt and module_ui function

---
+++ R/ui_general.R
@@ -0,0 +1,33 @@
+#' UI function for single module dashboard
+#'
+#' @param request shiny request object
+#' @param id Module id
+#' @param tab_item Tab item UI
+#'
+#' @return dashboardPage UI
+single_module_ui <- function(request, id, tab_item) {
+
+  DB_header <- shinydashboardPlus::dashboardHeader(
+    title = id
+  )
+  DB_sidebar <- shinydashboardPlus::dashboardSidebar(
+    shinydashboard::sidebarMenu(
+      id = "tablist",
+      shinydashboard::menuItem(id, tabName = id, selected = TRUE)
+    )
+  )
+  DB_body <- shinydashboard::dashboardBody(
+    htmltools::tags$script(src = "https://kit.fontawesome.com/01cb805c1e.js"),
+    shinydashboard::tabItems(
+      tab_item
+    )
+  )
+
+  shinydashboardPlus::dashboardPage(DB_header,
+                                    DB_sidebar,
+                                    DB_body,
+                                    #DB_rightsidebar,
+                                    #freshTheme = NCO_theme,
+                                    preloader = list(html = htmltools::tagList(waiter::spin_1(), "Loading ..."), color = "#367fa9"), # "#3c8dbc"),))
+  )
+}
