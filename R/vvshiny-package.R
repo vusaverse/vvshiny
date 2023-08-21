ec55760110772fb84939a2d9805c898500319113
Corneel den Hartogh <c.f.den.hartogh@vu.nl>
2023-08-21 09:36:08 +0200

    fix: use dplyr pipe instead of magrittr

--- R/vvshiny-package.R
+++ R/vvshiny-package.R
@@ -10,15 +10,15 @@ utils::globalVariables(c(":=", "!!", "."))
 
 #' Pipe operator
 #'
-#' See \code{magrittr::\link[magrittr:pipe]{\%>\%}} for details.
+#' See \code{dplyr::\link[dplyr:pipe]{\%>\%}} for details.
 #'
 #' @name %>%
 #' @rdname pipe
 #' @keywords internal
 #' @export
-#' @importFrom magrittr %>%
+#' @importFrom dplyr %>%
 #' @usage lhs \%>\% rhs
-#' @param lhs A value or the magrittr placeholder.
-#' @param rhs A function call using the magrittr semantics.
+#' @param lhs A value or the dplyr placeholder.
+#' @param rhs A function call using the dplyr semantics.
 #' @return The result of calling `rhs(lhs)`.
 NULL
